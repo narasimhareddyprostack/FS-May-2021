d93ef4a99296710ae854f544182189a4c891d61b
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-04-12 12:52:31 +0530

    tags

---
+++ classnotes.js
@@ -0,0 +1,49 @@
+All Tags Names:
+1. Document Tags 
+<html>
+<head>
+<body>
+2.Document Structure Tags 
+<section>
+<div>
+<header>
+<footer>
+<article>
+
+3. Test Formatting Tags 
+<h1>, <h6>
+<p> <span><bold> <strong>
+
+4.Link Tags 
+
+<a href=""></a>
+
+5. Image Tags 
+
+<img src=""/>
+6.List Tags
+<ul>  <ol>
+<li>
+
+7.Table 
+
+<table>
+<thead>
+<tbody>
+<tr>
+<th>
+<td>
+
+8.HTML 5 New Tags 
+9.Frame Tags 
+
+<iframe>
+
+10. Form Tags 
+<form>
+<label>
+<input>
+<textarea>
+<select>
+<option>
+
