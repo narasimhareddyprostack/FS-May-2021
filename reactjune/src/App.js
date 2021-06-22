739d59937fe040502b83c1d90fb02a03b3fe1abc
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-06-22 11:56:48 +0530

    react installation

---
+++ reactjune/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, GM</h1>
+      <h1>React Basics</h1>
+      <h1>React Basics</h1>
+      <h1>React Basics</h1> <h1>React Basics</h1> <h1>React Basics</h1>
+    </div>
+  );
+}
+
+export default App;
